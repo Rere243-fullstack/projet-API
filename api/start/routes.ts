e1d98d84b510dc30295eb5842704547af4db5401
Rere243-fullstack <rethambombo@gmail.com>
2025-09-20 14:59:28 +0200

    creation des routes

--- api/start/routes.ts
+++ api/start/routes.ts
@@ -8,9 +8,15 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
     hello: 'API mini projet',
   }
 })
+const UsersController = () => import('#controllers/users_controller')
+
+router.post('/register', [UsersController, 'register'])
+router.post('/login', [UsersController, 'login'])
+router.get('/profile', [UsersController, 'profile']).use(middleware.auth())
